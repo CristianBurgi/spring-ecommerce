e9f30f5a82574f396e4170df4720edd5b4db6b18
CristianBurgi <cristian.burgi@gmail.com>
2024-09-02 11:48:07 -0300

    Funcionalidad de guardar producto, vista create y LOGGER para clase productoController

---
+++ src/main/java/com/Cburgi/spring_ecommerce/repository/ProductoRepository.java
@@ -0,0 +1,12 @@
+package com.Cburgi.spring_ecommerce.repository;
+
+import com.Cburgi.spring_ecommerce.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto,Long> {
+
+
+
+}
---
+++ src/main/java/com/Cburgi/spring_ecommerce/service/ProductoService.java
@@ -0,0 +1,14 @@
+package com.Cburgi.spring_ecommerce.service;
+
+import com.Cburgi.spring_ecommerce.model.Producto;
+
+import java.util.Optional;
+
+public interface ProductoService {
+
+    public Producto save (Producto producto);
+    public Optional<Producto> get(Long id);
+    public void update (Producto producto);
+    public void delete (Long id);
+
+}
