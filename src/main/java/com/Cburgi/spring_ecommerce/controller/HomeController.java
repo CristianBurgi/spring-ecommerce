5ef1995c34b6bd8c4c62c84bd539c1731bdfdd7a
CristianBurgi <cristian.burgi@gmail.com>
2024-09-04 11:41:06 -0300

    Funcionalidad para mostrar el producto seleccionado en 'productohome'

--- src/main/java/com/Cburgi/spring_ecommerce/controller/HomeController.java
+++ src/main/java/com/Cburgi/spring_ecommerce/controller/HomeController.java
@@ -1,15 +1,21 @@
 package com.Cburgi.spring_ecommerce.controller;
 
 import com.Cburgi.spring_ecommerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    private final Logger log= LoggerFactory.getLogger(HomeController.class);
+
     @Autowired
     private ProductoService productoService;
 
@@ -18,6 +24,12 @@ public class HomeController {
 
         model.addAttribute("productos", productoService.findAll());
         return "usuario/home";
+    }
+
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Long id){
+        log.info("Id enviado cpmp párametro {}", id);
+        return "usuario/productohome";
     }
 
 
