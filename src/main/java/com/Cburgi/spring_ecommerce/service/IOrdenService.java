a1e6797aaa3de22b2b57e8bd67e0a7a660a9fb21
CristianBurgi <cristian.burgi@gmail.com>
2024-09-05 12:36:30 -0300

    Metodo para generar numero de orden

--- src/main/java/com/Cburgi/spring_ecommerce/service/IOrdenService.java
+++ src/main/java/com/Cburgi/spring_ecommerce/service/IOrdenService.java
@@ -2,8 +2,11 @@ package com.Cburgi.spring_ecommerce.service;
 
 import com.Cburgi.spring_ecommerce.model.Orden;
 
+import java.util.List;
+
 public interface IOrdenService {
     //metodos para la gestion de ordenes de compra
     Orden save(Orden orden);
+    List<Orden> findAll();
 
 }
--- src/main/java/com/Cburgi/spring_ecommerce/service/OrdenServiceImpl.java
+++ src/main/java/com/Cburgi/spring_ecommerce/service/OrdenServiceImpl.java
@@ -5,6 +5,10 @@ import com.Cburgi.spring_ecommerce.repository.IOrdenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 @Service
 public class OrdenServiceImpl implements IOrdenService {
     @Autowired
@@ -13,5 +17,37 @@ public class OrdenServiceImpl implements IOrdenService {
     @Override
     public Orden save(Orden orden) {
         return ordenRepository.save(orden);
+    }
+
+    @Override
+    public List<Orden> findAll() {
+        return ordenRepository.findAll();
+    }
+
+    public String generaNumeroOrden(){
+        int numero = 0;
+        String numeroConcatenado = "";
+        //Obtener el ultimo numero de orden
+
+        List<Orden> ordenes = findAll();
+
+        List<Integer> numeros = new ArrayList<Integer>();
+        ordenes.stream().forEach(orden ->numeros.add(Integer.parseInt(orden.getNumero())) );
+
+        if (ordenes.isEmpty()){
+            numero = 1;
+
+        }else{
+            numero = Collections.max(numeros);
+            numero++;
+        }
+
+        //Convertir numero a String con formato 000000000
+        numeroConcatenado = String.format("%09d", numero);
+        //Agregar el prefijo "000000"
+        numeroConcatenado = "000000" + numeroConcatenado;
+
+        //Devolver el numero concatenado con el prefijo "000000"
+        return numeroConcatenado;
     }
 }
